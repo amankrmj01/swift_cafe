49b99eea7914e89b73b98bf8b325045b762d4e7c
Aman Kumar Maurya <amankrmj@gmail.com>
2024-10-15 14:22:16 +0530

    basic floating nav bar

--- lib/presentation/home/bottom_nav_bar.dart
+++ lib/presentation/home/bottom_nav_bar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../consts/icons.dart';
+
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
 
@@ -8,20 +10,137 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      alignment: Alignment.center,
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      height: 60,
-      color: Colors.black.withOpacity(0.5),
+      height: 70,
+      decoration: ShapeDecoration(
+        color: Color(0xD6333333),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        shadows: [
+          BoxShadow(
+            color: Color(0x3F000000),
+            blurRadius: 4,
+            offset: Offset(0, 4),
+            spreadRadius: 0,
+          )
+        ],
+      ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Icon(Icons.home),
-          Icon(Icons.search),
-          Icon(Icons.add),
-          Icon(Icons.favorite),
-          Icon(Icons.person),
+          GestureDetector(
+            onTap: () => _onItemTapped(0),
+            child: _selectedIndex == 0
+                ? Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black.withOpacity(0.7),
+                      border: Border.all(
+                        color: Colors.brown,
+                        width: 2.0,
+                      ),
+                    ),
+                    child: SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: Image.asset(AppIcons.iconNav01Home),
+                    ),
+                  )
+                : Image.asset(AppIcons.iconNav01Home),
+          ),
+          GestureDetector(
+            onTap: () => _onItemTapped(1),
+            child: _selectedIndex == 1
+                ? Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black.withOpacity(0.7),
+                      border: Border.all(
+                        color: Colors.brown,
+                        width: 2.0,
+                      ),
+                    ),
+                    child: SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: Image.asset(AppIcons.iconNav02User),
+                    ),
+                  )
+                : Image.asset(AppIcons.iconNav02User),
+          ),
+          GestureDetector(
+            onTap: () => _onItemTapped(2),
+            child: _selectedIndex == 2
+                ? Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black.withOpacity(0.7),
+                      border: Border.all(
+                        color: Colors.brown,
+                        width: 2.0,
+                      ),
+                    ),
+                    child: SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: Image.asset(AppIcons.iconNav03Wallet),
+                    ),
+                  )
+                : Image.asset(AppIcons.iconNav03Wallet),
+          ),
+          GestureDetector(
+            onTap: () => _onItemTapped(3),
+            child: _selectedIndex == 3
+                ? Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black.withOpacity(0.7),
+                      border: Border.all(
+                        color: Colors.brown,
+                        width: 2.0,
+                      ),
+                    ),
+                    child: SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: Image.asset(AppIcons.iconNav04Bucket),
+                    ),
+                  )
+                : Image.asset(AppIcons.iconNav04Bucket),
+          ),
+          GestureDetector(
+            onTap: () => _onItemTapped(4),
+            child: _selectedIndex == 4
+                ? Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black.withOpacity(0.7),
+                      border: Border.all(
+                        color: Colors.brown,
+                        width: 2.0,
+                      ),
+                    ),
+                    child: SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: Image.asset(AppIcons.iconNav05Message),
+                    ),
+                  )
+                : Image.asset(AppIcons.iconNav05Message),
+          ),
         ],
       ),
     );
--- lib/presentation/home/home.screen.dart
+++ lib/presentation/home/home.screen.dart
@@ -1,163 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-
 import 'package:get/get.dart';
 import 'package:swift_cafe/consts/images.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-import '../../consts/app_detials.dart';
-import '../../consts/icons.dart';
 import 'bottom_nav_bar.dart';
 import 'controllers/home.controller.dart';
+import 'home_row_01.dart';
 
 class HomeScreen extends GetView<HomeController> {
   const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        statusBarIconBrightness: Brightness.light,
-      ),
-    );
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      bottomNavigationBar: BottomNavBar(),
       body: Stack(
         children: [
-          Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(AppImages.backgroundAuth01),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(AppImages.backgroundAuth02),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Opacity(
-            opacity: 0.50,
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment(0.00, -1.00),
-                  end: Alignment(0, 1),
-                  colors: [Color(0x004F71EA), Color(0x82CF7A4B)],
-                ),
-              ),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0x33000000),
-                  blurRadius: 24,
-                  offset: Offset(0, 4),
-                  spreadRadius: -1,
-                ),
-              ],
-              gradient: LinearGradient(
-                begin: Alignment(0.23, -0.97),
-                end: Alignment(-0.23, 0.97),
-                colors: [
-                  Colors.white.withOpacity(0.23000000417232513),
-                  Colors.white.withOpacity(0.11999999731779099),
-                  Colors.white.withOpacity(0.10999999940395355)
-                ],
-              ),
-            ),
-          ),
+          HomeLayer01(),
+          HomeLayer02(),
+          HomeLayer03(),
           SafeArea(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Flexible(
-                      flex: 1,
-                      child: Text(
-                        '👋',
-                        style: TextStyle(fontSize: 36),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            controller.currDate.value,
-                            style: TextStyle(
-                              color: Color(0xFFB5B5B5),
-                              fontSize: 13,
-                              fontFamily: 'Inter',
-                              fontWeight: FontWeight.w300,
-                              height: 0,
-                              letterSpacing: 0.20,
-                            ),
-                          ),
-                          Text(
-                            AppDetails.appName,
-                            style: TextStyle(
-                              color: Color(0xFFB5B5B5),
-                              fontSize: 18,
-                              fontFamily: 'Inter',
-                              fontWeight: FontWeight.w500,
-                              height: 0,
-                              letterSpacing: 0.20,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Flexible(
-                      flex: 1,
-                      child: Container(
-                        height: 40,
-                        width: 40,
-                        decoration: ShapeDecoration(
-                          color: Color(0xC9F8F8F8),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(7)),
-                          shadows: [
-                            BoxShadow(
-                              color: Color(0x0C000000),
-                              blurRadius: 20,
-                              offset: Offset(0, 5),
-                              spreadRadius: 0,
-                            )
-                          ],
-                        ),
-                        child: Image.asset(
-                          AppIcons.iconNav04Bucket,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      flex: 1,
-                      child: CircleAvatar(
-                        radius: 20,
-                        backgroundImage: AssetImage(AppImages.backgroundAuth01),
-                      ),
-                    ),
-                  ],
-                )
+                HomeRow01(controller: controller),
               ],
             ),
           ).p24().box.make(),
+          AnimatedPositioned(
+            child: BottomNavBar(),
+            duration: Duration(milliseconds: 500),
+            left: 20,
+            right: 20,
+            bottom: 20,
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class HomeLayer03 extends StatelessWidget {
+  const HomeLayer03({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Color(0x33FFFFFF),
+            blurRadius: 24,
+            offset: Offset(0, 4),
+            spreadRadius: -1,
+          ),
+        ],
+        gradient: LinearGradient(
+          begin: Alignment(0.23, -0.97),
+          end: Alignment(-0.23, 0.97),
+          colors: [
+            Colors.white.withOpacity(0.23000000417232513),
+            Colors.white.withOpacity(0.11999999731779099),
+            Colors.white.withOpacity(0.10999999940395355)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomeLayer02 extends StatelessWidget {
+  const HomeLayer02({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: 0.50,
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment(0.00, -1.00),
+            end: Alignment(0, 1),
+            colors: [Color(0x004F71EA), Color(0x82CF7A4B)],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomeLayer01 extends StatelessWidget {
+  const HomeLayer01({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AppImages.backgroundAuth01),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
